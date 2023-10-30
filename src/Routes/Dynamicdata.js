0516d3235e9b3ec03be9431b40bc4c9e11a3c623
Rohit Sen <130162361+Rohitsenji@users.noreply.github.com>
2023-10-30 11:50:23 +0530

    Update Dynamicdata.js
    more option added

--- src/Routes/Dynamicdata.js
+++ src/Routes/Dynamicdata.js
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
 import { Store } from "./Storedata";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams,Link } from "react-router-dom";
 
 const UsedataDynamically = () => {
   const id = useParams().id;
   const [Receiveddata] = useContext(Store);
+  // const filtered = Receivedata.filter((item) => item.category === datatype);
   const navigation = useNavigate();
 
   return(
@@ -23,6 +24,44 @@ const UsedataDynamically = () => {
         )
       })
       }
+      {/* More from the siren */}
+      <div className="dynamicmore" >
+        <h3 className="morefromsirentext" >
+          More from siren
+          <hr />
+        </h3>
+        <div className="Morearticles">
+          {Receiveddata.filter((item) => item.id % 14 === 0 && item.id <= 45).map(
+            (item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="HomeLatest"
+                >
+                  <Link to={`/Details/${item.id}`}>
+                    <div className="storiesimagecontainer">
+                    <img
+                      className="latestimage dynamicmoreimages"
+                      src={item.image}
+                      alt="Error"
+                      
+                    />
+                    </div>
+                    <div className="storiescontent">
+                      <p style={{ fontSize: "1rem" }}>{item.name}</p>
+                      <p style={{ fontSize: "1rem" }}>
+                        {item.text.slice(0, 20)}..
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              );
+            }
+          )}
+        </div>
+      </div>
+
+
     </>
   );
 };
